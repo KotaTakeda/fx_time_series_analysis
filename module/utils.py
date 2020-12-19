879289c378f6d2c8560bc84eeaf15bbd03c893a7
KotaTakeda <takeda11.kl@outlook.jp>
2020-12-19 16:19:56 +0900

    コメント追加

--- module/utils.py
+++ module/utils.py
@@ -10,14 +10,33 @@ from utils import *
 import numpy as np
 import pandas as pd
 
-# logの差分を取る関数とその逆関数
 def log_diff(arr):
+    """
+    対数値の差分をとる．
+    args: 
+        - arr: 配列
+    return:
+        - arrの初項の対数値とarrの対数差分
+    """
     return np.array([np.log(arr[0]), *np.diff(np.log(arr))])
 
 def log_diff_inv(log_diff_arr):
+    """
+    log_difの逆関数になっている．
+    args: 
+        - log_diff_arr: 配列, log_difで作った配列や対数差分をの空間で推定した配列
+    """
     return np.exp(np.cumsum(log_diff_arr), dtype=float)
 
 def load_fx_data(instrument_list, data_kind='train'):
+    """
+    fxデータをローカルから読み込み．
+    args:
+        - instrument_list: 文字配列, 為替ペア名の配列('USD_JPY', 'GBP_JPY', 'EUR_JPY')
+        - data_kind: 読み込みたいデータの種類．('train', 'test')
+    return:
+        df_fict: dict, 為替ペア名をkeyとしたfxデータの辞書
+    """
     df_dict = {}
     for instrument in instrument_list:
         df = pd.read_csv(f'data/fx_data_{instrument}_{data_kind}', index_col=0, header=0)
